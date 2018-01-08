fc49dce1ad9f5f4851f7febfd9ff187931124206
frohmanthenoob <cabal_357@hotmail.com>
2018-01-08 13:39:46 +0800

    autocomplete work
    add SearchTerms.js  and replace input in SearchBookComponent

--- src/SearchBooksComponent.js
+++ src/SearchBooksComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Autocomplete from 'react-autocomplete'
+import { SearchTerm } from './SearchTerm'
 /*
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
@@ -10,10 +12,12 @@ import sortBy from 'sort-by'
 class SearchBooks extends Component{
   
     state = {
-      books:[]
+      books:[],
+      value:''
     }
     
     render(){
+      
         return(
           <div>
             <div className="search-books">
@@ -28,13 +32,29 @@ class SearchBooks extends Component{
                     However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                     you don't find a specific author or title. Every search is limited by search terms.
                   */}
-                  <input type="text" placeholder="Search by title or author"/>
+                  
+                  <Autocomplete
+                  wrapperStyle = {{}}
+                  items={SearchTerm.map(myItem=>{return {id:myItem,label:myItem}})}
+                  shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                  getItemValue={item => item.label}
+                  renderItem={(item, highlighted) =>
+                    <div key={item.id} style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}} >
+                      {item.label}
+                    </div>
+                  }
+                  inputProps={{ placeholder: 'Search by title or author' }}
+                  value={this.state.value}
+                  onChange={e => this.setState({ value: e.target.value })}
+                  onSelect={value => this.setState({ value })}
+                  />
                 </div>
               </div>
               <div className="search-books-results">
                 <ol className="books-grid"></ol>
               </div>
             </div>
+            
           </div>
         )
     }
